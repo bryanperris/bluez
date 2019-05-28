d5d37e49c312541f1c8ea79d3ffc793574b9e1e4
Bryan Perris <bryanperris@live.com>
2019-05-28 11:35:36 -0400

    enable mesh support

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./configure --prefix=/opt/bluez --mandir=/opt/bluez/share/man --sysconfdir=/etc --localstatedir=/var
+./configure --prefix=/opt/bluez --mandir=/opt/bluez/share/man --sysconfdir=/etc --localstatedir=/var --enable-mesh
 make
 sudo make install
 sudo cp custom.bluetooth.service /etc/systemd/system/bluetooth.target.wants/bluetooth.service
