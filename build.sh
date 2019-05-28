a207416c1ac781f735c61384ee41ffafc3d1e2a0
Bryan Perris <bryanperris@live.com>
2019-05-28 12:31:23 -0400

    some script fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-./configure --prefix=/opt/bluez --mandir=/opt/bluez/share/man --sysconfdir=/etc --localstatedir=/var --enable-mesh
+./bootstrap-configure
+./configure --prefix=/opt/bluez --mandir=/opt/bluez/share/man --sysconfdir=/etc --localstatedir=/var --enable-mesh --enable-external-ell --with-package=ell
+make clean
 make
+sudo rm -rf /opt/bluez
 sudo make install
 sudo cp custom.bluetooth.service /etc/systemd/system/bluetooth.target.wants/bluetooth.service
 sudo systemctl daemon-reload
