c2b5ec4c55391a850ab32b4b198bf27afe211b31
Bryan Perris <7431407+bryanperris@users.noreply.github.com>
2021-10-16 12:05:30 -0400

    script fix

--- meshconfig.sh
+++ meshconfig.sh
@@ -2,5 +2,5 @@
 
 rm -rf ~/.config/meshctl
 mkdir -p ~/.config/meshctl
-cp tools/mesh/prov_db.json ~/.config/meshctl
-cp tools/mesh/local_node.json ~/.config/meshctl
+cp tools/mesh-gatt/prov_db.json ~/.config/meshctl
+cp tools/mesh-gatt/local_node.json ~/.config/meshctl
